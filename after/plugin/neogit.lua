28f4957475136aa2f8333c33e8ee5057f8db846e
Arjun Kakkar <arjunkakkar8@gmail.com>
2023-11-01 14:10:22 -0700

    Added barbar and neogit mappings

---
+++ after/plugin/neogit.lua
@@ -0,0 +1,4 @@
+vim.keymap.set('n', '<leader>d', ':DiffviewOpen<cr>')
+vim.keymap.set('n', '<leader>dc', ':DiffviewClose<cr>')
+vim.keymap.set('n', '<leader>h', ':DiffviewFileHistory<cr>')
+vim.keymap.set('n', '<leader>bh', ':DiffviewFileHistory --range=origin..HEAD<cr>')
