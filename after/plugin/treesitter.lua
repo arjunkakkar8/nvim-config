7346537d84a4fd86b85d2895cb486604db9111c8
arjunkakkar8 <arjunkakkar8@gmail.com>
2023-11-24 22:13:41 -0800

    Removed zig as compiler

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,5 +1,3 @@
-require 'nvim-treesitter.install'.compilers = { "zig" }
-
 require 'nvim-treesitter.configs'.setup {
     -- A list of parser names, or "all" (the five listed parsers should always be installed)
     ensure_installed = { "json", "yaml", "tsx", "markdown", "html", "svelte", "lua", "javascript", "typescript", "vim",
