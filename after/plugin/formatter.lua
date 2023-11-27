c7309370b62d8f8b84ef7c758fbc541c856fa8ba
arjunkakkar8 <arjunkakkar8@gmail.com>
2023-11-26 20:51:47 -0800

    Updated autoformat confid

--- after/plugin/formatter.lua
+++ after/plugin/formatter.lua
@@ -1,13 +1,13 @@
 require("conform").setup({
     formatters_by_ft = {
-        html = { "prettierd" },
-        svetle = { "prettierd" },
-        javascript = { "prettierd" },
-        scss = { "prettierd" },
+        html = { "prettierd", "prettier", "eslint_d" },
+        svetle = { "prettierd", "prettier", "eslint_d" },
+        javascript = { "prettierd", "prettier", "eslint_d" },
+        scss = { "prettierd", "prettier", "eslint_d" },
+        ["_"] = { "prettierd", "prettier", "eslint_d" },
     },
     format_on_save = {
         timeout_ms = 500,
         lsp_fallback = true,
     },
 })
-
