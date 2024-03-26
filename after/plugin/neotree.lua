c386ea663f743bde720fa9d5b6525bbeb350d67b
arjunkakkar8 <arjunkakkar8@gmail.com>
2024-03-26 12:29:42 -0700

    Simplified config, removed plugins and pared back neotree

--- after/plugin/neotree.lua
+++ after/plugin/neotree.lua
@@ -1,20 +1,17 @@
---nnoremap / :Neotree toggle current reveal_force_cwd<cr>
---nnoremap | :Neotree reveal<cr>
---nnoremap gd :Neotree float reveal_file=<cfile> reveal_force_cwd<cr>
---nnoremap <leader>b :Neotree toggle show buffers right<cr>
---nnoremap <leader>s :Neotree float git_status<cr>
-
 vim.keymap.set('n', '<leader>s', ':Neotree float git_status<cr>')
+vim.keymap.set('n', '<leader>t', ':Neotree float toggle reveal filesystem<cr>')
 
 require('neo-tree').setup({
     filesystem = {
+        window = {
+            position = 'current',
+        },
         filtered_items = {
             visible = true,
             hide_dotfiles = false,
         },
         follow_current_file = {
             enabled = true,
-        },
-        hijack_netrw_behaviour = "open_current",
+        }
     }
 })
