5ccb12055f4e2f8647b8e867b0096a6820409660
arjunkakkar8 <arjunkakkar8@gmail.com>
2024-05-28 17:53:14 -0700

    Updated lualine config, renamed colorscheme after

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,1 +1,10 @@
-require('lualine').setup()
+require('lualine').setup {
+    sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch' },
+        lualine_c = { 'filename' },
+        lualine_x = { 'filesize' },
+        lualine_y = {},
+        lualine_z = { { 'filetype', colored = false, icon_only = true } }
+    }
+}
