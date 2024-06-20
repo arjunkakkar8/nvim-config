b7e0a6d3ce77562304340270775531bd4d3d20e4
arjunkakkar8 <arjunkakkar8@gmail.com>
2024-06-20 10:35:22 -0700

    Updated config to include dap

--- after/plugin/dap.lua
+++ after/plugin/dap.lua
@@ -13,3 +13,29 @@ end
 dap.listeners.before.event_exited.dapui_config = function()
     dapui.close()
 end
+
+dap.configurations.rust = {
+    {
+
+        name = "Launch codelldb",
+        type = "codelldb",
+        request = "launch",
+        program = function()
+            return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/target/debug/", "file")
+        end,
+        cwd = "${workspaceFolder}",
+        stopOnEntry = false,
+        args = {},
+        runInTerminal = false,
+        showDisassembly = "always",
+    }
+}
+
+vim.keymap.set('n', '<leader>dn', dap.continue)
+vim.keymap.set('n', '<leader>dr', dap.restart)
+vim.keymap.set('n', '<leader>do', dap.step_over)
+vim.keymap.set('n', '<leader>di', dap.step_into)
+vim.keymap.set('n', '<leader>du', dap.step_out)
+vim.keymap.set('n', '<leader>db', dap.toggle_breakpoint)
+vim.keymap.set('n', '<leader>dt', dap.run_to_cursor)
+vim.keymap.set('n', '<leader>dD', dap.clear_breakpoints)
