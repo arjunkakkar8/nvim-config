96ef8afc90afa5dc30e9762470d02e12a488d71f
arjunkakkar8 <arjunkakkar8@gmail.com>
2024-12-06 15:09:38 -0800

    Updated config

--- after/plugin/neotree.lua
+++ after/plugin/neotree.lua
@@ -1,17 +1,16 @@
-vim.keymap.set('n', '<leader>s', ':Neotree float git_status<cr>')
 vim.keymap.set('n', '<leader>t', ':Neotree float toggle reveal filesystem<cr>')
 
 require('neo-tree').setup({
-    filesystem = {
-        window = {
-            position = 'current',
-        },
-        filtered_items = {
-            visible = true,
-            hide_dotfiles = false,
-        },
-        follow_current_file = {
-            enabled = true,
-        }
+  filesystem = {
+    window = {
+      position = 'current',
+    },
+    filtered_items = {
+      visible = true,
+      hide_dotfiles = false,
+    },
+    follow_current_file = {
+      enabled = true,
     }
+  }
 })
