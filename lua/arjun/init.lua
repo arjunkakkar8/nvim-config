3bae471e7887c1c5f24f94c05dde72a566601593
arjunkakkar8 <arjunkakkar8@gmail.com>
2025-01-13 13:00:30 -0800

    Updated packages and added tabclose remap

--- lua/arjun/init.lua
+++ lua/arjun/init.lua
@@ -28,11 +28,13 @@ vim.opt.updatetime = 50
 vim.opt.clipboard = "unnamedplus"
 
 if package.config:sub(1, 1) == '\\' then
-	vim.opt.shell = "cmd.exe"
+  vim.opt.shell = "cmd.exe"
 end
 
 vim.filetype.add({
-	extension = {
-		postcss = 'scss',
-	}
+  extension = {
+    postcss = 'scss',
+  }
 })
+
+vim.keymap.set("n", "tc", ":tabclose<CR>", { silent = true })
