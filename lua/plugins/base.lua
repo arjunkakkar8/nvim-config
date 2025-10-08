d7a37bdbc48396bd5eda4b2d7936d0a8a02f2112
Arjun Kakkar <arjunkakkar8@gmail.com>
2025-10-08 11:19:19 -0700

    package updates, moved to lazydev

--- lua/plugins/base.lua
+++ lua/plugins/base.lua
@@ -1,28 +1,27 @@
 return {
-    {
-        "folke/which-key.nvim",
-        event = "VeryLazy",
-        init = function()
-            vim.o.timeout = true
-            vim.o.timeoutlen = 300
-        end,
-        opts = {
-            -- your configuration comes here
-            -- or leave it empty to use the default settings
-            -- refer to the configuration section below
-        }
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    init = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
+    end,
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
+  },
+  { "folke/neoconf.nvim", cmd = "Neoconf" },
+  {
+    "folke/lazydev.nvim",
+    ft = "lua", -- only load on lua files
+    opts = {
+      library = {
+        -- See the configuration section for more details
+        -- Load luvit types when the `vim.uv` word is found
+        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+      },
     },
-    { "folke/neoconf.nvim", cmd = "Neoconf" },
-    {
-        "folke/neodev.nvim",
-        config = function()
-            require("neodev").setup({
-                library = {
-                    plugins = { "nvim-dap-ui" },
-                    types = true
-                },
-            })
-        end
-    },
-
+  },
 }
