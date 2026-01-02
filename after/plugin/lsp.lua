225730677e26bb308be6fa02a7b951c29c115b38
Arjun Kakkar <arjunkakkar8@gmail.com>
2026-01-02 12:54:55 -0800

    Lsp updates and other package updates

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,49 +1,43 @@
-local lsp_zero = require('lsp-zero')
-
-lsp_zero.on_attach(function(client, buff)
-  -- see :help lsp-zero-keybindings
-  -- to learn the available actions
-  lsp_zero.default_keymaps({
-    buffer = buff,
-    preserve_mappings = false
-  })
-end)
-
 -- LSPs to Enable
-
 require('mason').setup({
   PATH = "prepend"
 })
-require('mason-lspconfig').setup({
-  handlers = {
-    lsp_zero.default_setup,
+
+require("mason-lspconfig").setup({
+  ensure_installed = vim.g.lsps,
+  automatic_enable = true,
+})
+
+
+-- LSP Keybindings
+vim.api.nvim_create_autocmd('LspAttach', {
+  desc = 'LSP actions',
+  callback = function(event)
+    local opts = { buffer = event.buf }
+
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set("n", "gl", vim.diagnostic.open_float, opts) -- show diagnostics for line
+    vim.keymap.set('n', 'go', vim.lsp.buf.type_definition, opts)
+    vim.keymap.set("n", "gr", "<cmd>Telescope lsp_references<CR>", opts)
+  end,
+})
+
+local severity = vim.diagnostic.severity
+
+vim.diagnostic.config({
+  signs = {
+    text = {
+      [severity.ERROR] = " ",
+      [severity.WARN] = " ",
+      [severity.HINT] = "󰠠 ",
+      [severity.INFO] = " ",
+    },
   },
 })
 
-vim.lsp.config('cssls', {
-  settings = {
-    css = {
-      lint = {
-        unknownAtRules = "ignore",
-      },
-    }
-  }
-})
-vim.lsp.config('tailwindcss', {
-  settings = {
-    tailwindCSS = {
-      experimental = {
-        configFile = 'src/lib/styles/app.css',
-      },
-    }
-  }
-})
-
-
 -- Autocomplete actions
-
 local cmp = require('cmp')
-local cmp_action = require('lsp-zero').cmp_action()
 
 cmp.setup({
   mapping = cmp.mapping.preset.insert({
@@ -52,10 +46,6 @@ cmp.setup({
 
     -- Ctrl+Space to trigger completion menu
     ['<C-c>'] = cmp.mapping.complete(),
-
-    -- Navigate between snippet placeholder
-    ['<C-f>'] = cmp_action.luasnip_jump_forward(),
-    ['<C-b>'] = cmp_action.luasnip_jump_backward(),
 
     -- Scroll up and down in the completion documentation
     ['<C-u>'] = cmp.mapping.scroll_docs(-4),
--- lua/plugins/r.lua
+++ lua/plugins/r.lua
@@ -1,5 +1,5 @@
 return {
-   {
+  {
     "R-nvim/R.nvim",
     lazy = false,
     config = function()
@@ -25,23 +25,5 @@ return {
 
       require("r").setup(opts)
     end,
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    run = ":TSUpdate",
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = { "markdown", "markdown_inline", "r", "rnoweb" },
-        highlight = { enable = true },
-      })
-    end
-  },
-  "R-nvim/cmp-r",
-  {
-    "hrsh7th/nvim-cmp",
-    config = function()
-      require("cmp").setup({ sources = { { name = "cmp_r" } } })
-      require("cmp_r").setup({})
-    end,
-  },
+  }
 }
